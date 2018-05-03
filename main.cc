c8e03eb818dea203157ab984d870d012c34bc59d
Mohammad <the.momofier@gmail.com>
2018-05-03 02:17:59 -0700

    Skeleton of selection menu

---
+++ main.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	string user_input = "";
+	char indicator = 0;
+	loop = true;
+
+	cout << "What would you like to play?\nMinesweeper\nSnake\nTetris\nBlock breakers\n>";
+	getline(cin, user_input);
+
+	indicator = toupper(user_input.at(0));
+	
+	while(loop){	
+		switch(indicator){
+			case 'B':
+				//start Block Breakers
+				break;
+			case 'M':
+				//start Minesweeper
+				break;
+			case 'S':
+				//start Snake
+				break;
+			case 'T':
+				//start Tetris
+				break;
+			case 'Q':
+				loop = false;
+				break;
+			default:
+				cout << "Invalid input, try again.\nQuit will exit the game.\n";
+		}
+	}
+	return 0;
+}
